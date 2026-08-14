d6febba6fb43b14cd73c06529b93a4ce9a3f21b6
steve5535 <steve5535@naver.com>
2026-08-14 18:58:15 +0900

    student entity 작성

--- src/main/java/com/study/schoollostitemfinder/entity/Student.java
+++ src/main/java/com/study/schoollostitemfinder/entity/Student.java
@@ -1,4 +1,22 @@
 package com.study.schoollostitemfinder.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long studentId;
+    private long itemId;
+    private int studentNumber;
+    private String studentName;
+    private String takeAt;
 }
