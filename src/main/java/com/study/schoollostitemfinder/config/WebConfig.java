babff73a5a9d281d41bf97d207eb470741702a26
steve5535 <steve5535@naver.com>
2026-09-09 20:40:39 +0900

    2026-09-09

--- src/main/java/com/study/schoollostitemfinder/config/WebConfig.java
+++ src/main/java/com/study/schoollostitemfinder/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "/items/{itemId}",
                         "/items/take/{itemId}",
                         "/temporary-item",
-                        "/images/**"
+                        "/image/**"
                 );
     }
 
