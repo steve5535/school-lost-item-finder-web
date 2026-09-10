d1df83d815d87adf9193f204e638ee32d2c725e9
steve5535 <steve5535@naver.com>
2026-09-10 19:36:18 +0900

    2026-09-10

--- src/main/java/com/study/schoollostitemfinder/config/WebConfig.java
+++ src/main/java/com/study/schoollostitemfinder/config/WebConfig.java
@@ -33,7 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500", "https://school-lost-item-finder.vercel.app/")
+                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500", "https://school-lost-item-finder.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
